807416697d7d593d789d379ab3d54e30d25876f0
Yannick <yannick.boogaerts@technifutur.be>
2022-03-28 15:21:40 +0200

    ajout des lignes et colonnes dans une position

--- src/be/technifutur/labyrinthe/LabyrinthePosition.java
+++ src/be/technifutur/labyrinthe/LabyrinthePosition.java
@@ -3,4 +3,8 @@ package be.technifutur.labyrinthe;
 public interface LabyrinthePosition {
 
     LabyrinthePosition getPosition(Direction direction);
+
+    public int getLine();
+
+    public int getColumn();
 }
